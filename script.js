3ba520bd29a05f789f7c791eb053b796e4785d46
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-04-14 20:27:54 +0530

    css and js file completed

---
+++ script.js
@@ -0,0 +1,52 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+
+function inputLength() {
+	return input.value.length;
+}
+function crossOff(event) {
+	var target = event.target.parentNode;
+	target.classList.toggle("done");
+}
+
+function removeParent(event) {
+event.target.parentNode.remove();
+}
+
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+	li.innerHTML = " " + li.innerHTML + " ";
+
+	var check = document.createElement("button");
+	check.innerHTML = "Completed";
+	check.onclick = crossOff;
+
+	var button = document.createElement("button");
+	button.innerHTML = 'Remove';
+	button.onclick = removeParent;
+
+	li.prepend(check);
+	li.appendChild(button);
+
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
